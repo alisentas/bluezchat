176c178b124e838329c00301c3de7a80739e6bff
alisentas <alisentas96@gmail.com>
2016-07-27 13:37:15 +0300

    fix

--- discovery.py
+++ discovery.py
@@ -1,8 +1,9 @@
 import bluetooth
 import time
 from funcs import *
-from subprocess import call
+import sqlite
 from os import system
+import subprocess
 
 while True:
 	print "Discovery started."
@@ -12,22 +13,23 @@ while True:
 	peersArray = []
 	index = -1
 
+	processes = []
+
 	nearby_devices = bluetooth.discover_devices(8, True, True)
 
 	print nearby_devices
 
-	for device in nearby_devices:
-		peersArray.append([device[0], str(device[1]), 0])
-		print "Check: " + device[0] + " - " + device[1]
-		#call(["python send_discovery.py", device[0]])
-		system("python send_discovery.py %s \"%s\" &" % (device[0], device[1]))
-	   
 	peers = open("peers.txt", "w")
 
-	for peer in peersArray:
-		if peer[2]:
-			peers.write(peer[0] + "," + peer[1] + "\n")
+	for device in nearby_devices:
+		peersArray.append([device[0], str(device[1]), 0])
+		#call(["python send_discovery.py", device[0]])
+		#system("python send_discovery.py %s \"%s\" &" % (device[0], device[1]))
+		processes.append(subprocess.Popen(["python", "send_discovery.py", device[0], device[1]]))
 
-	peers.close()
+	for process in processes:
+		print "waiting"
+		process.wait()
+
 	print "Sleeping..."
-	time.sleep(20)
+	time.sleep(5)
--- send_discovery.py
+++ send_discovery.py
@@ -5,12 +5,12 @@ import sqlite
 bdaddr  = sys.argv[1]
 lookup_name = sys.argv[2]
 
+print "Check: " + bdaddr + " - " + lookup_name
+
 res = send_packet(bdaddr, 2, "What is the music of life?")
 if res:
     isopen = listen_packets(3)
     if isopen:
-        conn = sqlite.connect("peers.db")
-        c = conn.cursor()
-        c.execute("INSERT INTO peers VALUES (%s, %s)", bdaddr, lookup_name)
+        
 else:
     print "Couldn't connect"
