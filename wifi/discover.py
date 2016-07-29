1cf132d50518f45cf9c35f4a6c74de6e22ccb0ea
Caner Bakar <feanor924@gmail.com>
2016-07-29 09:33:29 +0300

    uploading
    discoverying IP's with threads

---
+++ wifi/discover.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python 
+
+
+import socket  
+
+import thread
+import time
+
+# Define a function for the thread
+
+thread_list = []
+
+def start_thread():
+	ip_ = 1
+	while ip_ < 254:
+		ip_ = ip_ + 1
+		IP = "172.16.5.%d", ip_;
+		# Create two threads as follows
+		try:
+			t = threading.Thread(discover,IP)
+			# Sticks the thread in a list so that it remains accessible
+			thread_list.append(t)
+		except:
+			print "Error: unable to start thread"
+
+	for thread in thread_list:
+		thread.start()
+
+	for thread in thread_list:
+		thread.join()
+
+	print "Done"
+
+
+def discover(IP):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket.settimeout(5.0)
+	host = socket.gethostname() # Get local machine name
+	port = 12345                # Reserve a port for your service.
+
+	server_address = ('IP', 12345)
+
+	try:
+		sock.connect(server_address)
+	except:
+		print " Error to discovering"
+
+
