6c812155aad3e0c90e1eaa39ea5e64e22cc25223
alisentas <alisentas96@gmail.com>
2016-08-04 11:11:38 +0300

    enc loop fix 9

--- bluezchat.py
+++ bluezchat.py
@@ -399,10 +399,11 @@ class BluezChatGui:
                     sock.send(data + "\t")
                 print "Data sent to that host"
                 return True
-            elif dest not in self.keys.keys():
-                conn.execute("INSERT INTO messages VALUES (?, ?, ?, ?)", (mtime, host, dest, message))
-                print "Messaged added to queue"
-                conn.commit()
+            else:
+                if dest not in self.keys.keys():
+                    conn.execute("INSERT INTO messages VALUES (?, ?, ?, ?)", (mtime, host, dest, message))
+                    print "Messaged added to queue"
+                    conn.commit()
                 self.send_all(4, mtime = mtime, host = host, dest = dest, message = message)
                 return True
         else:
